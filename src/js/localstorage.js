615d8e362e812222dd7e74184d3aa3fbd7aaf57f
BogdanLyopa <bogdanlyopa@gmail.com>
2021-01-22 14:23:34 +0200

    Initial commit

---
+++ src/js/localstorage.js
@@ -0,0 +1,7 @@
+
+
+// localStorage.setItem('theme',JSON.stringify(Theme));
+// const value = localStorage.getItem('theme');
+// console.log(value);
+// const parsedValue = JSON.parse(value)
+// console.log(parsedValue.DARK);
---
+++ src/js/templating.js
@@ -0,0 +1,7 @@
+import itemsTemplate from '../templates/product-items.hbs'
+import items from '../menu.json'
+
+
+const markup = itemsTemplate(items);
+const itemsRef = document.querySelector('.js-menu');
+itemsRef.insertAdjacentHTML('beforeend', markup);
